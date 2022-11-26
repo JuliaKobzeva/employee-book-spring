4150bb92c675e166c35a5071e1b8329a6ae44be1
Юля Кобзева <selemina@mail.ru>
2022-11-26 14:45:09 +0300

    пкрвый комит

---
+++ src/main/java/com/skypro/employeebooknew/EmployeeBookSpringNewApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.employeebooknew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookSpringNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBookSpringNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/employeebooknew/EmployeeBookSpringNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.employeebooknew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookSpringNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
